316fa53ad47340976999f6f19e7fdf982ec3deb5
sanjeet prasad verma <96581979+sanjeet145@users.noreply.github.com>
2025-11-18 15:46:55 +0530

    Delete utitlities directory

--- utitlities/__init__.py
+++
@@ -1,2 +0,0 @@
-from .logger import logging
-from .send_mail import send_email
--- utitlities/logger.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(filename)s - %(levelname)s - %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S",
-    handlers=[logging.FileHandler('app.log')]
-)
-
